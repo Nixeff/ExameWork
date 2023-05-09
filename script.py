38214212061484cb9cb79bddf7d515761dc94e99
Nixeff <theodore.folkesson@gmail.com>
2023-05-09 08:58:16 +0200

    createList now adds a empty Array to the file

--- script.py
+++ script.py
@@ -7,24 +7,60 @@ def getList(name):
         return json.load(file)
 
 def createList(name):
+    if (os.path.exists(r"ExameWork\Lists\ary_"+name+".json")):
+        print("That List allready exists")
+    else:
+        open(r"ExameWork\Lists\ary_"+name+".json", "x")
+        addData(name, [])
+    return
+
+def removeList(name):
+    if (os.path.exists(r"ExameWork\Lists\ary_"+name+".json")):
+        if(input("Are you sure? (y/n)") == "y"):
+            os.remove(r"Lists\ary_"+name+".json")
+        else:
+            print("")
+    else:
+        print("List dose not exist")
     return
 
 def getLists():
-    data = os.listdir(path="Lists")
-    print(len(data));
+    data = os.listdir(path=r"ExameWork\Lists")
     for i in range(len(data)):
         print((data[i].strip(".json")).strip("ary_"))
     return
+
+def addData(file, data):
+    if (os.path.exists(r"ExameWork\Lists\ary_"+file+".json")):
+        # Serializing json
+        json_object = json.dumps(data, indent=4)
+        
+        # Writing to sample.json
+        with open(r"ExameWork\Lists\ary_"+file+".json", "w") as outfile:
+            outfile.write(json_object)
+    return
+
+while True:    
+    
+    match input("Input:"):
+        case "loadList":
+            os.system('cls')
+            getLists()
+            data = getList(input("Which?:"))
+            print(data)
+        case "createList":
+            os.system('cls')
+            createList(input("Name?:"))
+        case "getLists":
+            os.system('cls')
+            getLists()
+        case "removeList":
+            os.system('cls')
+            getLists()
+            removeList(input("Name?:"))
+        case "end":
+            break
     
 
-match input():
-    case "loadList":
-        data = getList(input("Which?:"))
-        print(data)
-    case "createList":
-        print("Created List")
-    case "getLists":
-        getLists()
 
-    
     
