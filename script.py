772ad849d08a7d9dfd775f2683151e814a10b939
Nixeff <theodore.folkesson@gmail.com>
2023-05-11 11:54:50 +0200

    Its even nicer now

--- script.py
+++ script.py
@@ -4,27 +4,59 @@ import pyautogui
 from time import sleep
 from pyautogui import typewrite
 
-fileLoaded : bool = False
-activeLine : int = 1
-activeFile : str = ""
-activeData : dict = []
+class GlobalSettings():
+    def __init__(self):
+        self.fileLoaded : bool = False
+        self.activeLine : int = 1
+        self.activeFile : str = ""
+        self.activeData : dict = []
+    
+    def setFileLoaded(self, isItSet : bool):
+        self.fileLoaded = isItSet
+    def getFileLoaded(self):
+        return self.fileLoaded
+    
+    def setActiveLine(self,whatLine: int):
+        self.activeLine = whatLine
+    def getActiveLine(self):
+        return self.activeLine
+    
+    def setActiveFile(self,whatFile: str):
+        self.activeFile = whatFile
+    def getActiveFile(self):
+        return self.activeFile
+    
+    def setActiveData(self,whatData : dict):
+        self.activeData = whatData
+    def getActiveData(self):
+        return self.activeData
 
-def getList(name : str):
-    if (os.path.exists(r"ExameWork\Lists\ary_" + name + ".json")):
-        file_path = r"ExameWork\Lists\ary_" + name + ".json"
+settings = GlobalSettings()
+
+def getList():
+
+
+    if (os.path.exists(r"ExameWork\Lists\ary_" + settings.getActiveFile() + ".json")):
+        file_path = r"ExameWork\Lists\ary_" + settings.getActiveFile() + ".json"
         with open(file_path) as file:
-            return json.load(file)
+            settings.setFileLoaded(True)
+            settings.setActiveData(json.load(file))
+            
     else:
         print("File Dont exist")
+        settings.setActiveFile("")
         sleep(1)
-        return False
+
 
 def createList(name : str):
     if (os.path.exists(r"ExameWork\Lists\ary_"+name+".json")):
         print("That List allready exists")
     else:
         open(r"ExameWork\Lists\ary_"+name+".json", "x")
-        saveData(name, [])
+        settings.setActiveFile(name)
+        settings.setActiveData([])
+        saveData()
+        settings.setFileLoaded(True)
     return
 
 def removeList(name: str):
@@ -38,89 +70,109 @@ def removeList(name: str):
     return
 
 def getLists():
+
+
     data = os.listdir(path=r"ExameWork\Lists")
     for i in range(len(data)):
         print((data[i].strip(".json")).strip("ary_"))
     return
 
-def saveData(file: str, data: dict):
-    if (os.path.exists(r"ExameWork\Lists\ary_" + file + ".json")):
-        # Serializing json
-        json_object = json.dumps(data, indent=4)
-        
-        # Writing to sample.json
-        with open(r"ExameWork\Lists\ary_" + file + ".json", "w") as outfile:
+def saveData():
+    if (os.path.exists(r"ExameWork\Lists\ary_" + settings.getActiveFile() + ".json")):
+        json_object = json.dumps(settings.getActiveData(), indent=4)
+        with open(r"ExameWork\Lists\ary_" + settings.getActiveFile() + ".json", "w") as outfile:
             outfile.write(json_object)
         print("Save Sucssess")
     else:
-        print(file)
+        print(settings.getActiveFile())
         print("File Dose not Exist")
     return
 
-def addItem(pos : int, item : str):
-    activeData.insert(pos,item)
+def addItem(item : object):
+    indexForWhereToPutTheItem = settings.getActiveLine()-1
+    data = settings.getActiveData()
+    data.insert(indexForWhereToPutTheItem,item)
+    settings.setActiveData(data)
     return
 
-def removeItem(pos : int):
-    activeData.pop(pos)
+def removeItem():
+    data = settings.getActiveData()
+    data.pop(settings.getActiveLine()-1)
+    settings.setActiveData(data)
     return
 
-def editItem(pos:int):
-    typewrite(activeData[pos]["Name"])
-    activeData[pos]["Name"] = input("New Name:")
-    typewrite(str(activeData[pos]["Cost"]))
-    activeData[pos]["Cost"] = int(input("New Price:"))
+def editItem():
+    pos = settings.getActiveLine()-1
+    data = settings.getActiveData()
+    typewrite(data[pos]["Name"])
+    data[pos]["Name"] = input("New Name:")
+    typewrite(str(data[pos]["Price"]))
+    data[pos]["Price"] = int(input("New Price:"))
+    settings.setActiveData(data)
     return
 
-def init(fileLoaded,activeLine,activeFile,activeData):
+def writeData():
+    data = settings.getActiveData()
+    for i in range(len(data)):
+        if(i == settings.getActiveLine()-1):
+            print("> ("+str(i+1)+") "+str(data[i])+ " <")
+        else:
+            print("("+str(i+1)+") "+str(data[i]))
+
+def checkIfInputIsAViableNumber(userInput):
+    try:
+        userInput = int(userInput)
+        data = settings.getActiveData()
+        if(userInput < len(data)+1 and userInput >0):
+            settings.setActiveLine(userInput)
+        else:
+            print("No valid input given")
+            sleep(1)
+    except:
+        print("No valid input given")
+        sleep(1)
+    return
+
+def writeHelp():
+    print("-- Help Menu --")
+    print("(loadList) Lets you choose a list to work with")
+    print("(createList) Creates a new List")
+    print("(getLists) Get a list of all lists")
+    print("(removeList) Remove a list")
+    print("(end) Stops the program")
+
+def init():
     os.system('cls')
     print('Write "help" if you dont know what to do')
     while True:    
-        if(fileLoaded == True):
+        if(settings.getFileLoaded() == True):
             os.system('cls')
-            for i in range(len(activeData)):
-                if(i == activeLine-1):
-                    print("> ("+str(i+1)+") "+str(activeData[i])+ " <")
-                else:
-                    print("("+str(i+1)+") "+str(activeData[i]))
-            print("(a)Add Item (e)Edit Item (r)Remove Item (1-"+str(len(activeData))+")Change Active Item (s)Save (Exit)Exit file ")
-            iput = input(":")
-            match iput:
+            writeData()
+            print("(a)Add Item (e)Edit Item (r)Remove Item (1-"+str(len(settings.getActiveData()))+")Change Active Item (s)Save (Exit)Exit file ")
+            userInput = input(":")
+            match userInput:
                 case "a":
                     name = input("Name?:")
                     price = input("Price?:")
                     try:
                         price = int(price)
                         item = {"Name":name,"Price":price}
-                        addItem(activeLine-1,item)
+                        addItem(item)
                     except:
                         print("Price needs to be a number")
                         sleep(1)
                 case "e":
-                    editItem(activeLine-1)
+                    editItem()
                 case "r":
-                    removeItem(activeLine-1)
+                    removeItem()
                 case "s":
-                    saveData(activeFile,activeData)
-                    
+                    saveData()
                     sleep(1)
                 case "Exit":
                     os.system('cls')
-                    fileLoaded = False
+                    settings.setFileLoaded(False)
                 case _: 
-                    try:
-                        iput = int(iput)
-                        if(iput < len(activeData)+1 and iput >0):
-                            activeLine = iput
-                        else:
-                            print("No valid input given")
-                            sleep(1)
-                    except:
-                        print("No valid input given")
-                        sleep(1)
-                            
-
-                
+                    checkIfInputIsAViableNumber(userInput)
         else:
             match input("Input:"):
                 case "renameList":
@@ -132,14 +184,8 @@ def init(fileLoaded,activeLine,activeFile,activeData):
                 case "loadList":
                     os.system('cls')
                     getLists()
-                    activeFile = input("Which?:")
-                    data = getList(activeFile)
-                    if data == False:
-                        print("Not a valid file")
-                    else:    
-                        activeData = data
-                        fileLoaded = True
-                        print(data)
+                    settings.setActiveFile(input("Which?:"))
+                    getList()
                 case "createList":
                     os.system('cls')
                     createList(input("Name?:"))
@@ -152,16 +198,11 @@ def init(fileLoaded,activeLine,activeFile,activeData):
                     removeList(input("Name?:"))
                 case "help":
                     os.system('cls')
-                    print("-- Help Menu --")
-                    print("(loadList) Lets you choose a list to work with")
-                    print("(createList) Creates a new List")
-                    print("(getLists) Get a list of all lists")
-                    print("(removeList) Remove a list")
-                    print("(end) Stops the program")
+                    writeHelp()
                 case "end":
                     break
                 case _:
                     print('Write "help" to get valid commands')
         
 
-init(fileLoaded,activeLine,activeFile,activeData)
+init()
